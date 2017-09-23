2e70cf38d47546f9acde6c918d3de26c055d8bb7
Conor Hartigan <conorhartigan13@gmail.com>
2017-09-23 14:44:06 +0100

    inital commit

---
+++ game.js
@@ -0,0 +1,8 @@
+let score = 0;
+let cookieElement = document.getElementById('cookie');
+let scoreElement = document.getElementById('score');
+
+cookieElement.addEventListener('click', function(){
+    score++;
+    scoreElement.textContent = score + "points";
+});
